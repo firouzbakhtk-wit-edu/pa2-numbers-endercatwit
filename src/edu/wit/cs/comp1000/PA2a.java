9aa08fbd8ecce3353b4c33324d28622135cb8bc5
cynth <cynth@10.220.33.15>
2024-10-04 14:29:39 -0400

    added main code

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,105 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+/*Cynthia Ender
+ * sums and averages
+ */
 public class PA2a {
-
-	// TODO: document this function
+	//main function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Enter five whole numbers: ");
+		int a = input.nextInt();
+		int b = input.nextInt(); //it would be better to make this an array,
+		int c = input.nextInt(); //but i don't know how
+		int d = input.nextInt(); //and you don't want me to
+		int e = input.nextInt();
+		int smp = 0;
+		int smn = 0;
+		double avp = 0;
+		double avn = 0;
+		int cnp = 0;
+		int cnn = 0;
+		//if there's only one number in a category, you make it singular. if there are none, you print 0
+		//print only 2 decimal places - use %.2f
+		if (a > 0) {
+			smp += a; //whee... can't iterate properly...
+			avp += a;
+			cnp ++;
+		} else {      //arrays? we don't need no arrays
+			smn += a;
+			avn += a;
+			cnn ++;
+		}
+		if (b > 0) {
+			smp += b;
+			avp += b;
+			cnp ++;
+		} else {
+			smn += b;
+			avn += b;
+			cnn ++;
+		}
+		if (c > 0) {
+			smp += c;
+			avp += c;
+			cnp ++;
+		} else {
+			smn += c;
+			avn += c;
+			cnn ++;
+		}
+		if (d > 0) {
+			smp += d;
+			avp += d; //this sucks
+			cnp ++;
+		} else {
+			smn += d;
+			avn += d;
+			cnn ++;
+		}
+		if (e > 0) {
+			smp += e;
+			avp += e;
+			cnp ++;
+		} else {
+			smn += e;
+			avn += e;
+			cnn ++;
+		}
+		
+		if (cnp == 1) {
+			System.out.printf("The sum of the 1 positive number: %d%n", smp);
+		} else {
+			System.out.printf("The sum of the %d positive numbers: %d%n", cnp, smp);
+		}
+		if (cnn == 1) { //checks in case it needs to singularize
+			System.out.printf("The sum of the 1 non-positive number: %d%n", smn);
+		} else {
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", cnn, smn);
+		}
+		System.out.printf("The sum of the 5 numbers: %d%n", a+b+c+d+e);
+		
+		if (cnp == 0) {
+			System.out.printf("The average of the 0 positive numbers: 0.00%n");
+		} else if (cnp == 1) {
+			System.out.printf("The average of the 1 positive number: %.2f%n", avp);
+		} else {
+			avp = avp / cnp;
+			System.out.printf("The average of the %d positive numbers: %.2f%n", cnp, avp);
+		}
+		if (cnn == 0) { //there are many checks to do
+			System.out.printf("The average of the 0 non-positive numbers: 0.00%n");
+		} else if (cnn == 1) {
+			System.out.printf("The average of the 1 non-positive number: %.2f%n", avn);
+		} else {
+			avn = avn / cnn;
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", cnn, avn);
+		}
+		System.out.printf("The average of the 5 numbers: %.2f%n", (0.0+a+b+c+d+e)/5);
+		
+		input.close();
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,28 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
+import java.util.Scanner;
+/*Cynthia Ender
+ * finds roots of quadratic equations
+ */
 public class PA2b {
 
-	// TODO: document this function
+	//main function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		//remember, roots are found using (-b plus or minus sqrt(d))/2a
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Enter a b c: ");
+		double a = input.nextDouble(); //takes input
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		double d = (b * b) - (4* a * c); //finds the discriminant
+		if (d < 0) {
+			System.out.printf("Roots: imaginary%n");
+			System.exit(0);
+		} else if (d == 0) {
+			System.out.printf("Root: %.2f%n", (0.0 - b)/(2 * a));
+		} else {
+			System.out.printf("Roots: %.2f, %.2f%n", (0.0 - b - Math.sqrt(d))/(2 * a), (0.0 - b + Math.sqrt(d))/(2 * a));
+		} //that one's long, it finds the roots
+		input.close();
 	}
 
 }
